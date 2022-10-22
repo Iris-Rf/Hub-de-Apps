04d3aabcacb8f5986a5e56dbbd4d397748fcc602
Iris-Rf <irys@hotmail.com>
2022-10-22 21:33:03 +0200

    select

--- app/components/input/input.js
+++ app/components/input/input.js
@@ -1,27 +1,29 @@
-// import { home } from "../../pages/home";
+// import { divPokemon } from "../../pages/pokeapi/pokeapi";
 
-// export const user = () => {
-//   const app = document.querySelector("#app");
-
-//   app.innerHTML = `
-//     <div class="containerUser">
-//         <h1 class="title">GAMES</h1>
-//         <h2 class="inputUserName">Enter your name</h2>
-//         <input type="text" id="inputUserName">
-//         <button id="btnUserName">Click here!</button>
-//     </div>
-//     `;
-
-//   const localUserName = () => {
-//     localStorage.setItem("userName", input.value);
-//     home();
-//   };
-
-// export const home = () =>{
-//   const app = document.querySelector('#app');
-//   }
-
-//   const btnUserName = document.querySelector("#btnUserName");
-//   const input = document.querySelector("input");
-//   btnUserName.addEventListener("click", () => localUserName(input.value)); 
+// let guardarPokemonsType = [];
+// export const getPokemonsSelect = async () => {
+//   try {
+//       const response = await fetch(`https://pokeapi.co/api/v2/type/`);
+//       const dataJson = await response.json();
+//       transformDt(dataJson);
+//       guardarPokemonsType.push(dataJson);
+//       console.log(guard);
+//     }
+//    catch (error) {}
 // };
+
+// export const transformDt = (list) => {
+//   const containerOptions = document.querySelector("#selectPokemon")
+//  list.forEach((type) => {
+//   containerOptions.innerHTML += divPokemon(type.name);
+//  }
+//  )
+// };
+
+//   export const printPokemosSelect = (list) => {
+//     for (const item of list) {
+//       return`
+//       <option class="typePokemon">${item.name}</option>
+//       `;
+//     }
+//   };
--- app/pages/pokeapi/pokeapi.js
+++ app/pages/pokeapi/pokeapi.js
@@ -1,25 +1,28 @@
 import { cleanPage } from "../../utils/cleanPage";
-import "./pokeapi.css";
 
+import "./pokeapi.css";
+let optionsSelectPoke = [];
 let filtro = [];
 export const pokeapi = async () => {
   const container = document.querySelector("#app");
   container.innerHTML = divPokemon();
   const inputPokemon = document.querySelector("#inputPokemon");
   inputPokemon.addEventListener("input", filterPokemons);
+  await getPokemons();
+  getSelectOptions();
   filterPokemons();
-  getPokemons();
 };
 
+// <label for="labelSelect">Pokemon type</label>
 export const divPokemon = () => {
   return `
   <div class="buscador">
     <div class="label">
-    <label class="labelInfo" id="labelInfo">BUSCADOR</label>
+      <label class="labelInfo" id="labelInfo">SEARCH</label>
     </div>
-    <input type="text" id="inputPokemon" class="inputPokemon">
-    <ul id="resultado">
-    </ul>
+      <input type="text" id="inputPokemon" class="inputPokemon">
+      <select id="selectPokemon" class="selectPokemon" name="PUTOTYPES">
+      </select>
     </div>
   <div class="containerPokemon" id="containerPokemon"></div>
   `;
@@ -37,15 +40,12 @@ const getPokemons = async () => {
       const dataJson = await response.json();
       pokemons.push(dataJson);
       guardarPokemons.push(dataJson);
-
     }
     transformData(pokemons);
-  } catch (error) {
-  }
-
+  } catch (error) {}
 };
 
-export const transformData = (list) =>{
+export const transformData = (list) => {
   //guardar mapeo en filtro
   filtro = list.map((item) => ({
     image: item.sprites.other.home.front_default,
@@ -55,10 +55,17 @@ export const transformData = (list) =>{
     height: item.height,
   }));
 
+  let selectPoke = new Array();
+  selectPoke = filtro.map((item) => item.type);
+  const setTypes = new Set(selectPoke);
+  selectPoke = Array.from(setTypes);
+  optionsSelectPoke = selectPoke;
+  console.log(optionsSelectPoke);
   printPokemos(filtro);
-}
+  return optionsSelectPoke;
+};
 
-export const  printPokemos = (list) => {
+export const printPokemos = (list) => {
   const box = document.querySelector("#containerPokemon");
   for (const item of list) {
     const template = `
@@ -72,21 +79,58 @@ export const  printPokemos = (list) => {
     `;
     box.innerHTML += template;
   }
-}
-
-export const filterPokemons = () => {
-
-const text = inputPokemon.value.toLowerCase();
-const box = document.querySelector("#containerPokemon");
-cleanPage(box);
-// guarda los resultados que se incluyen en el listado filtrado
-let resultadoGuardar = [];
-console.log(guardarPokemons);
-guardarPokemons.forEach((param) => {
-  if (param.name.includes(text)) {
-    resultadoGuardar.push(param);
-  }
-});
-transformData(resultadoGuardar);
 };
 
+export const filterPokemons = () => {
+  const text = inputPokemon.value.toLowerCase();
+  const box = document.querySelector("#containerPokemon");
+  cleanPage(box);
+  // guarda los resultados que se incluyen en el listado filtrado
+  let resultadoGuardar = [];
+  // console.log(guardarPokemons);
+  guardarPokemons.forEach((param) => {
+    if (param.name.includes(text)) {
+      resultadoGuardar.push(param);
+    }
+  });
+  transformData(resultadoGuardar);
+};
+
+// FILTRO POR TIPO
+
+// export const getSelect = async () => {
+//   try {
+//       const response = await fetch(`https://pokeapi.co/api/v2/type/`);
+//       const dataJson = await response.json();
+//       console.log(dataJson);
+//       transformDt(dataJson);
+
+//     }
+//    catch (error) {}
+// };
+
+export const getSelectOptions = ()=> {
+  const containerOptions = document.getElementById("selectPokemon");
+  // console.log(containerOptions);
+  console.log(optionsSelectPoke);
+  // for (const types of selectPoke) {
+  //   console.log(types);
+  //   containerOptions.innerText += printPokemosSelect(types);
+  // }
+  // selectPoke = transformData();
+  optionsSelectPoke.forEach((type) => {
+    console.log("hola");
+    console.log(type);
+    containerOptions.innerHTML += templetaOptionsSelect(type);
+  });
+}
+
+export let filterTypeOptions = () => {
+  selectPokemon
+} 
+
+export const templetaOptionsSelect = (item) => {
+  return `
+      <option value="${item}">${item}</option>
+      `;
+};
